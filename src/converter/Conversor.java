f37b200b2f16a4ab71c04c6c51da016ad0f3d55f
77dav <david.77va@gmail.com>
2023-03-06 16:36:43 -0300

    Add Coversor.java file

---
+++ src/converter/Conversor.java
@@ -0,0 +1,23 @@
+package converter;
+
+public class Conversor {
+	
+	String entrada;
+	double salida;
+	
+	public double convertirPesoADolar() {
+		return 0;
+	}
+	public double convertirPesoAEuro() {
+		return 0;
+	}
+	public double convertirPesoALibrasEsterlinas() {
+		return 0;
+	}
+	public double convertirPesoAYen() {
+		return 0;
+	}
+	public double convertirPesoAWonSulCoreano() {
+		return 0;
+	}
+}
